42884b893a562f718f0f1efdf09e9b6a55a35d01
Rafał Piotrowski <rafal.piotrowski@mindmade.pl>
2022-08-18 15:12:08 +0200

    solution added

---
+++ story_time.js
@@ -0,0 +1,38 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let greeting = 'Hello adventurer!';
+let name = 'Frodo';
+let title = 'hobbit';
+let worldName = 'Mordor';
+
+let age = 135;
+let year = 2300;
+let century = 24;
+
+let items = ['Staff', 'Sword', 'Axe'];
+let nicePerson = true;
+
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+
+console.log(greeting + ' My name is ' + name + '.');
+console.log('I am the most important ' + title + ' of the world.')
+console.log('I am ' + age + ' years old' + ' and coming to you from year ' + year + ' so from ' + century + 'th century.');
+console.log('I will explain you what items I can use:')
+console.log('I like using ' + items[0] + ' but I can also use ' + items[1] + ' or ' + items[2])
+console.log('Help me on me quest destroying the ring!')
+
+
